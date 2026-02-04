04938b6bb1d6469e23510565ca52b1148979ed2c
Mouad Bensafir <mouadensafir@gmail.com>
2026-02-04 22:04:10 +0100

    Handle Admin API Shutdown too

--- Reverse_Proxy/main.go
+++ Reverse_Proxy/main.go
@@ -5,6 +5,9 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"os"
+	"os/signal"
+	"context"
 
 	"github.com/MouadBensafir/SafeNode/internal/admin"
 	"github.com/MouadBensafir/SafeNode/internal/config"
@@ -25,6 +28,12 @@ func main() {
 	adminMux.HandleFunc("/backends", admin.BackendsHandler(mainPool))
 	adminMux.HandleFunc("/status", admin.StatusHandler(mainPool))
 
+    adminAddr := ":" + fmt.Sprint(cfg.AdminPort)
+    adminServer := &http.Server{
+        Addr:    adminAddr,
+        Handler: adminMux,
+    }
+
 	// Proxy endpoint
 	proxyAddr := ":" + fmt.Sprint(cfg.Port)
 	proxyServer := &http.Server{
@@ -34,22 +43,48 @@ func main() {
 		WriteTimeout: time.Duration(cfg.RequestTimeout) * time.Millisecond,
 		IdleTimeout:  time.Duration(cfg.RequestTimeout) * 12 * time.Millisecond,
 	}
-	err := proxyServer.ListenAndServeTLS("cert.pem", "key.pem")
-	if err != nil {
-		log.Fatal(err)
-	}
+
+	// Strat Proxy
+	go func() {
+		log.Printf("Starting proxy on %s", proxyAddr)
+		err := proxyServer.ListenAndServeTLS("cert.pem", "key.pem")
+		if err != nil {
+			log.Fatal("Proxy server error: ", err)
+		}
+	} ()
 
 	// Start Health Checker
 	go healthcheck.StartHealthChecker(cfg.HealthCheckFreq, cfg.HealthCheckPath, mainPool)
 
-	//Serve the Admin API
+	// Start the Admin API
 	go func() {
 		adminAddr := ":" + fmt.Sprint(cfg.AdminPort)
 		log.Printf("Starting Admin API on %s", adminAddr)
-		err := http.ListenAndServeTLS(adminAddr, "cert.pem", "key.pem", adminMux)
+		err := adminServer.ListenAndServeTLS("cert.pem", "key.pem")
 		if err != nil {
 			log.Fatal(err)
 		}
 	}()
 
+	// Handle Interrupt Signal
+	graceful_exit_signal := make(chan os.Signal)
+	signal.Notify(graceful_exit_signal, os.Interrupt)
+	<-graceful_exit_signal
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+
+	// Shutdown Proxy
+    if err := proxyServer.Shutdown(ctx); err != nil {
+        log.Printf("Proxy forced to shutdown: %v", err)
+    } else {
+        log.Println("Proxy stopped gracefully")
+    }
+
+    // Shutdown Admin API
+    if err := adminServer.Shutdown(ctx); err != nil {
+        log.Printf("Admin API forced to shutdown: %v", err)
+    } else {
+        log.Println("Admin API stopped gracefully")
+    }
 }
